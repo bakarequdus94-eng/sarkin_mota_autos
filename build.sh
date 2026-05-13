e9aa88c57de30fed8036252ba6bc6c0ca5678868
bakarequdus94-eng <bakarequdus94@gmail.com>
2026-05-13 20:06:33 +0000

    Automate superuser creation for free tier

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+# This creates the superuser automatically using the environment variables above
+python manage.py createsuperuser --noinput || true
