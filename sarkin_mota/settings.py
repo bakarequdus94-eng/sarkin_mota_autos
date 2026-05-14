a233f2b926ac4519fdc3ae1975dbfeb0f12d742b
bakarequdus94-eng <bakarequdus94@gmail.com>
2026-05-14 18:06:35 +0000

    Fix os NameError and settings syntax

--- sarkin_mota/settings.py
+++ sarkin_mota/settings.py
@@ -92,9 +92,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dcrqxoh29',
+    'CLOUD_NAME': "dcrqxoh29",
     'API_KEY': '335587852872787',
-    'API_SECRET': 'SvKevc1z8FJ4ZnpI3cIfcnXEeB0',
+    'API_SECRET': "SvKevc1z8FJ4Zmpi3cIfcnXEeB0",
 }
 
 MEDIA_URL = 'https://res.cloudinary.com/dcrqxoh29/'
