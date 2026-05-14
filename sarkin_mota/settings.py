37ac07042c4d3d172011cfda8dc25b012b9df474
bakarequdus94-eng <bakarequdus94@gmail.com>
2026-05-14 18:56:00 +0000

    Move cloudinary_storage to top of INSTALLED_APPS

--- sarkin_mota/settings.py
+++ sarkin_mota/settings.py
@@ -91,11 +91,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # For serving static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': "dcrqxoh29",
-    'API_KEY': '335587852872787',
-    'API_SECRET': "SvKevc1z8FJ4Zmpi3cIfcnXEeB0",
-}
-
-MEDIA_URL = 'https://res.cloudinary.com/dcrqxoh29/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = '/media/'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dcrqxoh29',
+    'API_KEY': '335587852872787',
+    'API_SECRET': 'SvKevc1z8FJ4ZnpI3cIfcnXEeB0',
+}
