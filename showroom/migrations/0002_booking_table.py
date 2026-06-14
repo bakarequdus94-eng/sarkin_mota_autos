50b1d0c46f6c85435d6cd245b7710171200b2abf
bakare qudus <bakarequdus94@gmail.com>
2026-06-14 19:16:27 +0000

    Fix: Changed max_name arguments to max_length in migration schema

--- showroom/migrations/0002_booking_table.py
+++ showroom/migrations/0002_booking_table.py
@@ -12,11 +12,11 @@ class Migration(migrations.Migration):
             name='InspectionBooking',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
+                ('full_name', models.CharField(max_length=255)),
                 ('phone_number', models.CharField(max_length=20)),
                 ('email', models.EmailField(max_length=254)),
                 ('preferred_date', models.DateField()),
-                ('preferred_time', models.CharField(max_name=20)),
+                ('preferred_time', models.CharField(max_length=20)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('car', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='bookings', to='showroom.car')),
             ],
